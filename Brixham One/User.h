65644899d68380091f7f1a59420198d0782bd7f8
Александр Чегошев <4egoshev@gmail.com>
2017-09-11 05:16:13 +0700

    add user model

---
+++ Brixham One/User.h
@@ -0,0 +1,15 @@
+//
+//  User.h
+//  Brixham One
+//
+//  Created by Александр Чегошев on 11.09.17.
+//  Copyright © 2017 Александр Чегошев. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@property (strong, nonatomic) NSString *token;
+
+@end
---
+++ Brixham One/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  Brixham One
+//
+//  Created by Александр Чегошев on 11.09.17.
+//  Copyright © 2017 Александр Чегошев. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
