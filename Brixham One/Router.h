9f7f0dbb33a2c791a354f5c3fa025eb098309625
Александр Чегошев <4egoshev@gmail.com>
2017-09-11 03:16:51 +0700

    add router

---
+++ Brixham One/Router.h
@@ -0,0 +1,24 @@
+//
+//  Router.h
+//  Brixham One
+//
+//  Created by Александр Чегошев on 08.09.17.
+//  Copyright © 2017 Александр Чегошев. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef enum {
+    LoginRequest = 0,
+    SitesRequest
+} RequestType;
+
+@interface Router : NSObject
+
+@property (assign, nonatomic) RequestType requestType;
+
++ (Router *)sharedManager;
+
+- (NSString *)stringWithrequestType:(RequestType)requestType;
+
+@end
---
+++ Brixham One/Router.m
@@ -0,0 +1,42 @@
+//
+//  Router.m
+//  Brixham One
+//
+//  Created by Александр Чегошев on 08.09.17.
+//  Copyright © 2017 Александр Чегошев. All rights reserved.
+//
+
+#import "Router.h"
+
+@implementation Router
+
++ (Router *)sharedManager {
+
+    static Router *manager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        manager = [Router new];
+    });
+    return manager;
+}
+
+- (NSString *)stringWithrequestType:(RequestType)requestType {
+
+    NSString *baseURL = @"http://nerine.space:8000/";;
+    NSString *method = @"";
+
+    switch (requestType) {
+        case LoginRequest:
+            method = @"api/user/api-token-auth/";
+            break;
+
+        default:
+            break;
+    }
+
+    return [NSString stringWithFormat:@"%@%@",baseURL,method];
+}
+
+
+
+@end
